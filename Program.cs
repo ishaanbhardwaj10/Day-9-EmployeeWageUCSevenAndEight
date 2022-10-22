b269aeb40aa86f84ef4ce352a141c912c38c9b31
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-22 17:49:13 +0530

    added UC-8 option to switch case in main method

--- Program.cs
+++ Program.cs
@@ -1,52 +1,25 @@
-﻿using System;
-
-namespace DayEightEmployeeWage
+﻿namespace EmployeeWage
 {
     class Program
     {
-        public const int IS_FULL_TIME = 2;
-        public const int IS_PART_TIME = 1;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUMBER_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_A_MONTH = 100;
-
-        public static int ComputeEmployeeWage()
-        {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
-
-            Random random = new Random();
-            int empWage = 0, empHrs = 0, totalEmpWage = 0, totalWorkingHrs = 0, day = 0;
-
-            while (totalWorkingHrs <= MAX_HRS_IN_A_MONTH && day < NUMBER_OF_WORKING_DAYS)
-            {
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Daily employee wage: " + empWage);
-                totalWorkingHrs += empHrs;
-                day++;
-            }
-            Console.WriteLine("Total working hours in the month: {0}", totalWorkingHrs);
-            Console.WriteLine("Total number of working days: {0}", day);
-            Console.WriteLine("Total employee wage for the month is: {0}", totalEmpWage);
-            return totalEmpWage;
-        }
-
         public static void Main(string[] args)
         {
-            ComputeEmployeeWage();
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Console.WriteLine("Use Case #8: Employee Wage for Multiple Companies");
+            Console.Write("Choose a use case number to run the program: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 8:
+                    EmployeeWageMultipleCompanies employeeWageMultipleCompanies = new EmployeeWageMultipleCompanies();
+                    employeeWageMultipleCompanies.CalculateMonthlyWage("Flipkart", 20, 10, 60);
+                    employeeWageMultipleCompanies.CalculateMonthlyWage("Amazon", 20, 15, 80);
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid use case number!");
+                    break;
+            }
         }
     }
 }
